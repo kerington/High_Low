9e5ffebfb3ca8790df48808f8c89869fa5a89ceb
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-03-15 16:14:05 -0500

    Adding skeleton copy of highLowGame

---
+++ highLowGame.php
@@ -0,0 +1,10 @@
+<?php
+
+// random number
+
+// if random umber greater
+	// elseif lower
+		// else equal
+
+// $userguess = fgets(STDIN) - reads what user types
+	// $userguess === random number variable
